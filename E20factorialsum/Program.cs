04af84ba3a7c403efc0b49ca7d571f1d82e64ee3
PhoebeHoku <95447747+PhoebeHoku@users.noreply.github.com>
2023-09-12 17:43:03 -0400

    E20

---
+++ E20factorialsum/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+//get sum of 100! and then sum the individual digits
+var t0 = new Stopwatch();
+
+
+t0.Start();
+
+
+t0.Stop();
+Console.WriteLine($"Answer: in {t0.ElapsedMilliseconds} MS");
